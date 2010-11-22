c90503a597a35d5641c2b3250a29c83e20593557
David Petrie <david@iMac27-2.local>
2010-11-22 17:44:49 +1300

    python crap

---
+++ python/cwd.py
@@ -0,0 +1,3 @@
+import os, sys
+
+print os.path.dirname(os.path.abspath(sys.argv[0]))
---
+++ python/encodingTest.py
@@ -0,0 +1,25 @@
+from HTMLFilter import HTMLDecode
+from HTMLFilter import HTMLEncode
+
+
+
+
+	
+
+
+
+# d = HTMLDecode("&#8220; &#8217;").encode("utf8")
+print ReplaceLatinExtended("&#8220; &#8217;")
+
+
+
+
+
+
+
+
+
+
+
+
+	
